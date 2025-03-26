b928bdf25a2eb1aa2db4eddc457942ab6d5edcc1
Wingerted <wingerted@gmail.com>
2025-03-26 21:52:17 +0800

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# toml11-feedstock
+
+This is the feedstock for the [toml11](https://github.com/ToruNiina/toml11) package.
+
+## About toml11
+
+toml11 is a C++11 header-only toml parser/encoder library. It provides a simple and efficient way to parse and generate TOML files in C++ applications.
+
+## Building from source
+
+To build this package from source:
+
+```bash
+conda build conda.recipe
+```
+
+## License
+
+This feedstock is licensed under the MIT License, as is the original toml11 package.
---
+++ conda.recipe/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+set -x
+
+
+# 显式指定使用 conda 环境中的 clang 和 clang++（此步可作为保险，同时 PATH 也已修改）
+export CC="$BUILD_PREFIX/bin/clang"
+export CXX="$BUILD_PREFIX/bin/clang++"
+
+mkdir -p build && cd build
+
+cmake ${CMAKE_ARGS} -GNinja \
+    -DCMAKE_PREFIX_PATH="$PREFIX" \
+    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+    -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -fpic" \
+    -DREFLECTCPP_USE_BUNDLED_DEPENDENCIES=OFF \
+    -DREFLECTCPP_USE_VCPKG_DEFAULT=OFF \
+    -DREFLECTCPP_USE_VCPKG=OFF \
+    -DREFLECTCPP_FLEXBUFFERS=ON \
+    -DREFLECTCPP_MSGPACK=ON \
+    -DREFLECTCPP_XML=ON \
+    -DREFLECTCPP_TOML=ON \
+    -DREFLECTCPP_YAML=ON \
+    "$SRC_DIR"
+
+ninja
+
+# The `install` command will take care of copying the files to the right place
+ninja install
